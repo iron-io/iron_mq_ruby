9da6d13a0bdcdc358ab114828511717c95fa2d44
Travis Reeder <travis@appoxy.com>
2011-11-04 20:04:45 -0700

    Using concur.

--- test/long_run.rb
+++ test/long_run.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'concur'
 begin
   require File.join(File.dirname(__FILE__), '../lib/ironmq')
 rescue Exception => ex
@@ -13,25 +14,36 @@ end
 
 start = Time.now
 puts "Queuing #{@num_to_add} items at #{start}..."
+executor = Concur::Executor.new_thread_pool_executor(50)
 @num_to_add.times do |i|
-	puts "POST #{i}..."
-	res = @client.messages.post("hello world! #{i}")
-	p res
+  task = executor.execute do
+    begin
+      puts "POST #{i}..."
+      res = @client.messages.post("hello world! #{i}")
+    rescue => ex
+      puts "ERROR! #{ex.class.name}: #{ex.message} -- #{ex.backtrace.inspect}"
+      raise ex
+    end
+  end
 end
+
 put_time = (Time.now.to_f - start.to_f)
 puts "Finished pushing in #{put_time} seconds"
+
+exit if true
 
 start = Time.now
 puts "Getting and deleting #{@num_to_add} items at #{start}..."
 @num_to_add.times do |i|
-    puts "GET #{i}..."
-    res = @client.messages.get()
-    p res
-    puts "DELETE #{i}..."
-    res = @client.messages.delete(res["id"])
-    p res
+  puts "GET #{i}..."
+  res = @client.messages.get()
+  p res
+  puts "DELETE #{i}..."
+  res = @client.messages.delete(res["id"])
+  p res
 end
 
 puts "Finished pushing #{@num_to_add} items in #{put_time} seconds."
 puts "Finished getting and deleting #{@num_to_add} items in #{(Time.now.to_f - start.to_f)} seconds."
 
+executor.shutdown
