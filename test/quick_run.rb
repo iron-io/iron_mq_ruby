fc907885bd8f10ebde53938ec4d3547c319514f7
Travis Reeder <treeder@gmail.com>
2011-12-29 22:01:45 -0800

    Added some assertions.

--- test/quick_run.rb
+++ test/quick_run.rb
@@ -2,27 +2,32 @@
 require_relative 'test_base'
 
 class QuickRun < TestBase
- 
- def setup
-  super
-   @client.queue_name = 'ironmq-gem-quick'
-end
+
+  def setup
+    super
+    @client.queue_name = 'ironmq-gem-quick'
+  end
 
   def test_basics
     res = @client.messages.post("hello world!")
+    assert res.id
+    assert res.msg
     p res
 
     res = @client.messages.get()
+    assert res.id
+    assert res.body
     p res
 
     res = @client.messages.delete(res["id"])
+    assert res.msg
     p res
 
     res = @client.messages.get()
     p res
-   
+
   end
 
- 
+
 end
 
