f0c8de5b9cff2028c62da0c2495e5fbd69d40fd0
Travis Reeder <treeder@gmail.com>
2012-01-04 15:28:19 -0800

    Passes all options in messages.get/post to the get/post params.

--- lib/iron_mq/messages.rb
+++ lib/iron_mq/messages.rb
@@ -16,11 +16,7 @@ module IronMQ
     #  :timeout => amount of time before message goes back on the queue
     def get(options={})
       begin
-        params = nil
-        if options[:timeout]
-          params = {:timeout => options[:timeout]}
-        end
-        res, status = @client.get(path(options), params)
+        res, status = @client.get(path(options), options)
         return Message.new(self, res)
       rescue IronMQ::Error => ex
         if ex.status == 404
@@ -34,8 +30,12 @@ module IronMQ
 
     # options:
     #  :queue_name => can specify an alternative queue name
+    #  :delay => time to wait before message will be available on the queue
+    #  :timeout => The time in seconds to wait after message is taken off the queue, before it is put back on. Delete before :timeout to ensure it does not go back on the queue.
+    #  :expires_in => After this time, message will be automatically removed from the queue.
     def post(payload, options={})
-      res, status = @client.post(path(options), :body=>payload)
+      options[:body] = payload
+      res, status = @client.post(path(options), options)
       return Message.new(self, res)
     end
 
