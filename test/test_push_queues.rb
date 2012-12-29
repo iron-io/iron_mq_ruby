5ae732df2d80e34b39f4c1b5f5d96fb525d85c3b
Alexander Kolesen <akolesen@iron.io>
2012-12-29 06:40:56 +0300

    Fixed test_failure ('reserved' instead of 'retrying')
    It's too hard to catch 'retrying' here. This state could be catched only
    if pusherd timeout is larger, than retry delay (reserved_until almost always in future).

--- test/test_push_queues.rb
+++ test/test_push_queues.rb
@@ -124,7 +124,7 @@ class TestPushQueues < TestBase
     subscribers.each do |s|
       p s
       assert_equal 503, s["status_code"]
-      assert_equal "retrying", s["status"]
+      assert_equal "reserved", s["status"]
     end
     puts 'sleeping 20'
     sleep 20
