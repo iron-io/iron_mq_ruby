29d25bb33bff37b0abb2aa7ea085a6da690fd659
Travis <treeder@gmail.com>
2013-03-18 15:55:57 -0700

    Ignore 404 on deletes. Sometimes retries causing double deletes so 404 on.

--- lib/iron_mq/messages.rb
+++ lib/iron_mq/messages.rb
@@ -28,7 +28,15 @@ module IronMQ
     end
 
     def delete
-      call_api_and_parse_response(:delete)
+      begin
+        call_api_and_parse_response(:delete)
+      rescue Rest::HttpError => ex
+        if ex.code == 404
+          Rest.logger.info("Delete got 404, safe to ignore.")
+        else
+          raise ex
+        end
+      end
     end
 
     def call_api_and_parse_response(meth, ext_path = "", options = {}, instantiate = true)
