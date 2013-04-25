e462cf360c553500ad3e150d2afb6a8251102da2
Travis <treeder@gmail.com>
2013-04-24 17:20:07 -0700

    Test for converting to back to pull type.

--- test/test_push_queues.rb
+++ test/test_push_queues.rb
@@ -391,4 +391,28 @@ class TestPushQueues < TestBase
     assert_equal 200, resp.code, "API must response with HTTP 200 status, but returned HTTP #{resp.code}"
   end
 
+
+  # tests when converting a push queue back to a pull queue
+  def test_converting_types
+    queue = @client.queue('converting_queue')
+    subscribers = [{:url => "http://rest-test.iron.io/code/200"},
+                   {:url => "http://rest-test.iron.io/code/200"}]
+
+    res = queue.update_queue(:subscribers => subscribers,
+                             :push_type => 'multicast')
+    queue.reload
+
+    assert_equal "multicast", queue.push_type
+
+    p queue.update_queue(:push_type => 'pull')
+
+    queue.reload
+
+    p queue.push_type
+
+    assert_equal nil, queue.push_type
+
+  end
+
+
 end
--- test/tmp.rb
+++ test/tmp.rb
@@ -9,27 +9,6 @@ class IronMQTests < TestBase
 
   end
 
-  def test_converting_types
-    queue = @client.queue('converting_queue')
-    subscribers = [{:url => "http://rest-test.iron.io/code/200"},
-                   {:url => "http://rest-test.iron.io/code/200"}]
-
-    res = queue.update_queue(:subscribers => subscribers,
-                             :push_type => 'multicast')
-    queue.reload
-
-    assert_equal "multicast", queue.push_type
-
-    p queue.update_queue(:push_type => 'pull')
-
-    queue.reload
-
-    p queue.push_type
-
-    assert_equal "pull", queue.push_type
-
-  end
-
 
 end
 
